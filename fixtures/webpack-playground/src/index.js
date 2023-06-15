0a1e5a19a95f7a74b18745b037396bcaaa1d7cc0
qinduanyu <610744313@qq.com>
2023-06-15 23:02:20 +0800

    feat(demo): counter

--- fixtures/webpack-playground/src/index.js
+++ fixtures/webpack-playground/src/index.js
@@ -6,8 +6,8 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode key={'StrictMode'}>
+    <App key={'App'} />
   </React.StrictMode>
 );
 
