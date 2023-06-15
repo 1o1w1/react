5be4b91be5c2243da861ff9a776dfbaa146389cc
qinduanyu <610744313@qq.com>
2023-06-15 21:24:53 +0800

    feat: link script

---
+++ fixtures/webpack-playground/scripts/link.sh
@@ -0,0 +1,9 @@
+# ln -s ../../../build/node_modules/react/umd/react.development.js ../public/react.development.js
+# ln -s ../../../build/node_modules/react/umd/react.development.js.map ../public/react.development.js.map
+# ln -s ../../../build/node_modules/react-dom/umd/react-dom.development.js ../public/react-dom.development.js
+# ln -s ../../../build/node_modules/react-dom/umd/react-dom.development.js.map ../public/react-dom.development.js.map
+
+ln -s ./build/node_modules/react/umd/react.development.js ./public/react.development.js
+ln -s ./build/node_modules/react/umd/react.development.js.map ./public/react.development.js.map
+ln -s ./build/node_modules/react-dom/umd/react-dom.development.js ./public/react-dom.development.js
+ln -s ./build/node_modules/react-dom/umd/react-dom.development.js.map ./public/react-dom.development.js.map
